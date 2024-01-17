7ceea0964ef4ff65b4e80a4f241999a2c7dd3c61
Juanmunoz8 <mun231047@uvg.edu.gt>
2024-01-16 19:09:41 -0600

    radio-primer-trabajo

--- radio/src/main/java/com/Logic/classes/Radio.java
+++ radio/src/main/java/com/Logic/classes/Radio.java
@@ -1,5 +1,21 @@
 package com.Logic.classes;
 
 public class Radio {
-    
+    private boolean encendido;
+
+    public Radio() {
+        this.encendido = false;
+    }
+
+    public void encender() {
+        this.encendido = true;
+    }
+
+    public void apagar() {
+        this.encendido = false;
+    }
+
+    public boolean estaEncendido() {
+        return this.encendido;
+    }
 }
