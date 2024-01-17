971bbc923441d28852ee38fcaebc09853105e0f1
PeDro0210 <pedroruben0210@gmail.com>
2024-01-16 19:29:26 -0600

    Radio Template

--- radio/src/main/java/com/Logic/classes/Radio.java
+++ radio/src/main/java/com/Logic/classes/Radio.java
@@ -1,21 +1,75 @@
 package com.Logic.classes;
-
-public class Radio {
+import com.Logic.interfaces.IRadio;
+public class Radio implements IRadio {
     private boolean encendido;
+    protected String marca;
+    private int banda;
+    private float estacion;
+    private int volume;
+    private float[] SaveStation = new float[12];
 
     public Radio() {
         this.encendido = false;
+        this.marca = "MLJP";
+        this.banda = IRadio.AM;
+        this.estacion = 530;
+        this.volume = 0;
     }
 
-    public void encender() {
-        this.encendido = true;
+    public void setEncendido(boolean encendido) {
+        this.encendido = encendido;
     }
 
-    public void apagar() {
-        this.encendido = false;
+    public void setVolume(int volume) {
+        if (volume >= 0 && volume <= 100) {
+            this.volume = volume;
+        } else {
+            this.volume = 0;
+        }
     }
 
-    public boolean estaEncendido() {
+    public void setEstacion(float emisora, int Sentbanda) {
+        if (Sentbanda == banda){
+            this.estacion = emisora;
+        }
+    }
+
+    public void setBanda() {
+        if (this.banda == IRadio.AM) {
+            this.banda = IRadio.FM;
+        } else {
+            this.banda = IRadio.AM;
+        }
+    }
+
+    public Boolean getEncendido() {
         return this.encendido;
     }
+
+    public float getEstacion() {
+        return this.estacion;
+    }
+
+    public int getBanda() {
+        return this.banda;
+    }
+
+    public int getVolume() {
+        return this.volume;
+    }
+
+    public void guardarEstacion(float emisora, int banda, int indx) {
+        for (int i = 0; i < this.SaveStation.length; i++) {
+            if (i == indx) {
+                this.SaveStation[i] = emisora;
+                break;
+            }
+        }
+    }
+
+    public float recuperarEstacion(int indx) {
+        return this.SaveStation[indx];
+    }
+
+
 }
